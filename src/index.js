7ce77098e636ae088d7d86ee523bf82c0d77ae23
cinis-ad-cinis <valentinakhramtsova@gmail.com>
2023-01-31 15:45:55 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length%2) {
+    return false
+  }
+  let stack = [];
+  const obj = Object.fromEntries(bracketsConfig);
+
+  for (let i = 0; i < str.length; i++) {
+    if (stack.length != 0 && obj[stack[stack.length - 1]] === str[i]) {
+      stack.pop();
+    } else {
+      if (obj.hasOwnProperty(str[i])) {
+        stack.push(str[i])
+      } else {
+        return false
+      }
+    }
+  }
+  return stack.length === 0;
 }
